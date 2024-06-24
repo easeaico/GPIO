02836ee1c4897c485d8ff769e0f77a022ba53f09
harry <yelcat@gmail.com>
2024-06-24 21:20:41 +0800

    feat: add wiring pi

--- Sources/Wiring/Wiring.swift
+++ Sources/Wiring/Wiring.swift
@@ -2,15 +2,50 @@ import Foundation
 import wiringPi
 
 public enum WiringErrors : Error {
-    case openChipErr
-    case getLineErr
+    case setupGpioErr
 }
 
-public class Wiring {
+@inlinable public func wiringSetupGpio(){
+    wiringPiSetupGpio()
+}
 
-    public init() throws {
-        wiringPiSetupGpio()
+public class LED {
+    let pin: Int32
+
+    public init(forPin number: Int32) {
+        self.pin = number
+        pinMode(self.pin, OUTPUT)
     }
+
+    public func on() {
+        digitalWrite(self.pin, HIGH)
+    }
+
+    public func off() {
+        digitalWrite(self.pin, LOW)
+    }
+}
+
+public class Button {
+        let pin: Int32
+
+        public init(forPin number: Int32) {
+            self.pin = number
+            pinMode(self.pin, INPUT)
+            pullUpDnControl(self.pin, PUD_UP)
+        }
+
+        public func onPress() -> Bool {
+            let val = digitalRead(self.pin)
+            return val == LOW
+        }
+
+        public func onRelease() -> Bool {
+            let val = digitalRead(self.pin)
+            return val == HIGH
+        }
+
+
 
 }
 
--- Tests/WiringTests/WiringTests.swift
+++ Tests/WiringTests/WiringTests.swift
@@ -2,7 +2,14 @@ import XCTest
 @testable import Wiring
 
 final class WiringPiTests: XCTestCase {
-    func testWiringPiLed() throws {
-        let w = Wiring()
+    func testWiringButton() {
+        wiringSetupGpio()
+        let btn = Button(forPin: 0)
+        for _ in 0...10 {
+            if btn.onPress() {
+                XCTFail("")
+            }
+            Thread.sleep(forTimeInterval: 1)
+        }
     }
 }
